5dcf39959f04a68af637e57d2342048e57ab275b
bengant3 <bengant3@gmail.com>
2021-12-29 20:48:21 -0500

    completed basic card component and basic task page component

--- components/card.tsx
+++ components/card.tsx
@@ -1,5 +1,6 @@
 // Copyright 2021 Ben
 
+import { flexbox } from "@mui/system";
 import { useState } from "react";
 
 type tag = {
@@ -7,16 +8,16 @@ type tag = {
     color: string;
 }
 
-type cardProps = {
+export type cardProps = {
     cardTitle: string;
     cardNotes: string;
     cardTags: tag[];
 }
 
-const Tag = ({name, color="gray"}: tag) =>{
+const Tag = (prop : tag) => {
     return(
-        <div>
-            <p>{name}</p>
+        <div style={{...tagStyle, ...{backgroundColor: prop.color}}}>
+            <p style ={tagText}>{prop.name}</p>
         </div>
     )
 }
@@ -24,21 +25,34 @@ const Tag = ({name, color="gray"}: tag) =>{
 const Card = ({cardTitle, cardNotes, cardTags}: cardProps) => {
     const [title, setTitle] = useState(cardTitle);
     const [notes, setNotes] = useState(cardNotes);
-    const [tags, setTags] = useState(cardTags); //custom hook to add/remove a tag
+    const [tags, setTags] = useState(cardTags); //custom hook to add/remove a tag?
 
     return(
-        <div>
-            <div>
-                <p>{title}</p>
-                <div>
-                    {tags.map((t: tag) =>
-                        <div> <p>{t.name}</p> </div>
-                        )}
+        <div style={cardStyle}>
+            <div style={cardContent}>
+                <p style={titleStyle}>{title}</p>
+                <div style={tagContainer}>
+                    {tags.map((t: tag) => 
+                        <Tag name={t.name} color={t.color === "" ? "gainesboro": t.color}/>)}
                 </div>
-                <p>{notes}</p>
+                <p style = {noteText}>{notes}</p>
             </div>
         </div>
     )
 }
+
+const cardStyle = {
+    border: "2 solid gray", 
+    borderRadius: 5
+};
+const cardContent = {
+    display: "flex", 
+    flexDirection: "column" as "column"
+};
+const titleStyle = {fontWeight: "bold"}
+const tagContainer = {display: "flex"}
+const tagStyle = {borderRadius: 5}
+const tagText = {fontSize: 12}
+const noteText = {}
 
 export default Card;
--- pages/tasks.tsx
+++ pages/tasks.tsx
@@ -4,6 +4,30 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import {  } from '@mui/material';
 import {  } from '@mui/icons-material'
+import Card, { cardProps } from '../components/card'
+
+type boardProps = {
+    boardTitle: string;
+    boardCards: cardProps[];
+}
+
+const Board = ({boardTitle, boardCards} : boardProps) => {
+    const [title, setTitle] = useState(boardTitle);
+    const [cards, setCards] = useState(boardCards); //custom hooks to add/remove cards??
+
+    return(
+        <div>
+            <div>
+                <p>{title}</p>
+                <div>
+                    {cards.map((c: cardProps) => 
+                        <Card cardTitle={c.cardTitle} cardNotes={c.cardNotes} cardTags={c.cardTags}/>)}
+                </div>
+            </div>
+        </div>
+
+    );
+}
 
 const TasksPage = () => {
     const [failedAttempt, setFailedAttempt] = useState(false);
@@ -13,7 +37,9 @@ const TasksPage = () => {
 
     return(
         <div>
-
+            <div>
+                
+            </div>
         </div>
     );
 }
